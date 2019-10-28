813f0f48d10f1070b60ad7cbf65c8206be5fbfd2
Kaniah Dunn <kaniahdunn@gmail.com>
2019-10-28 16:26:35 -0400

    first commit

--- week-8/codewars-week-8-solutions.js
+++ week-8/codewars-week-8-solutions.js
@@ -3,9 +3,9 @@
 
 function getGrade (s1, s2, s3) {
     // Code here
-  
+
     let score = (s1 + s2 +s3) / 3
-    
+
     if (score >= 90 && score <= 100 ) {
       return 'A';
     }else if (score >= 80 &&  score <= 90){
@@ -18,3 +18,20 @@ function getGrade (s1, s2, s3) {
       return 'F';
     }
   }
+
+  // Grasshopper - Make Change
+  // Get change
+  // You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
+  // 
+  // Item	Cost
+  // Candy	$1.42
+  // Chips	$2.40
+  // Soda	$1.00
+  // Create 5 variables and use the cost from the table above to set their values.
+  // Add code here
+
+  let candy = 1.42
+  let chips = 2.40
+  let soda = 1.00
+  let money = 10
+  let change = money - (candy + chips + soda)
