db7efa4e3bf3ae85effd2b483b99371ff8ac24a6
Kaniah Dunn <kaniahdunn@gmail.com>
2019-10-30 23:22:02 -0400

    first commit

--- week-8/codewars-week-8-solutions.js
+++ week-8/codewars-week-8-solutions.js
@@ -22,7 +22,7 @@ function getGrade (s1, s2, s3) {
   // Grasshopper - Make Change
   // Get change
   // You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
-  // 
+  //
   // Item	Cost
   // Candy	$1.42
   // Chips	$2.40
@@ -37,7 +37,7 @@ function getGrade (s1, s2, s3) {
   let change = money - (candy + chips + soda)
 
 
-//Drink About 
+//Drink About
 // Kids drink toddy.
 // Teens drink coke.
 // Young adults drink beer.
@@ -69,3 +69,35 @@ function peopleWithAgeDrink(old) {
     return 'drink whisky'
   }
 };
+
+// A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+// Our friend Pac has finally decided to pursue a career in the coding industry.
+// He is a newbie, he needs to learn properly.
+// Therefore, Pac wants to apply for the worldwide famous -and very demanding-
+// 'C0d3r 1ns1d3' Academy for beginners.
+// In order to join, Pac is required to solve a series of 3 exercises about 'Bug Fixes'.
+// He has been sent an email from the Academy with the following instructions,
+//
+// Dear Pac,
+// This is the first exercise. Find out what's wrong and fix the code.
+// You have 15 minutes to send a solution.
+// Good Luck.
+// This code is a mess! Would you help Pac to fix the code in time?
+//
+// (This might be helpful -> Math.Random();)
+//
+// This is my first Kata, so please any feedback (especially on Test Cases) is welcome!
+
+function yourFutureCareer () {
+	var career = Math.random()
+		if (career <= 0.32) {
+			return 'FrontEnd Developer'
+	  }else if (career <= 0.65) {
+			  return 'BackEnd Developer'
+		} else {
+			  return 'Full-Stack Developer'
+		}
+    };
+
+
+yourFutureCareer();
