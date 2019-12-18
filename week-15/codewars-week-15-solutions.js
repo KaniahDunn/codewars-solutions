5c69ebf49d9ccd251327b4276334884b420aedf2
Kaniah Dunn <kaniahdunn@gmail.com>
2019-12-18 11:26:48 -0500

    daily solution

--- week-15/codewars-week-15-solutions.js
+++ week-15/codewars-week-15-solutions.js
@@ -36,3 +36,20 @@ function countPositivesSumNegatives(input) {
     }
     return array;
 }
+
+// Get the Mean of an Array
+//
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+//
+// Return the average of the given array rounded down to its nearest integer.
+//
+// The array will never be empty.
+
+
+function getAverage(marks){
+  var count = 0;
+  for(var i =0; i < marks.length; i++){
+    count += marks[i]
+  }
+  return(Math.floor(count/marks.length))
+}
