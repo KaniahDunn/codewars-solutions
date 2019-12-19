56a95870a9f393eb3bfa9af93d1e7f5d2977c924
Kaniah Dunn <kaniahdunn@gmail.com>
2019-12-19 13:05:26 -0500

    daily codewars solution

--- 12.js
+++ 12.js
@@ -65,3 +65,27 @@ function getAverage(marks){
 function invert(array) {
    return array.map((v) => v > 0 ? -v : Math.abs(v));
 }
+
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
+//
+//   sumDigits(10);  // Returns 1
+//   sumDigits(99);  // Returns 18
+//   sumDigits(-32); // Returns 5
+// Let's assume that all numbers in the input will be integer values.
+//
+
+function sumDigits(number){
+var toStr = number.toString();
+var count = 0;
+if (toStr[0] === '-'){
+for (let i = 1; i < toStr.length; i++){
+  count +=  parseInt(toStr[i])
+}
+}else{
+  for (let i = 0; i < toStr.length; i++){
+  count +=  parseInt(toStr[i])
+}
+}
+return count
+}
