2a84b15718c5b16ebf96bffa82c09c9d76cac13d
Kaniah Dunn <kaniahdunn@gmail.com>
2019-12-20 11:03:13 -0500

    daily codewars solution

--- 12.js
+++ 12.js
@@ -89,3 +89,17 @@ for (let i = 1; i < toStr.length; i++){
 }
 return count
 }
+
+
+// Sum Mixed Array
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//
+// Return your answer as a number.
+
+function sumMix(x){
+  var count = 0 ;
+for(var i = 0; i < x.length; i++){
+   count +=  parseInt(x[i])
+}
+  return count
+}
