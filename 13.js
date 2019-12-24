72dd80278b8aff0087ceb11da7417ce7bb9926f1
Kaniah Dunn <kaniahdunn@gmail.com>
2019-12-24 11:49:12 -0500

    daily solution

--- 13.js
+++ 13.js
@@ -31,3 +31,12 @@ function findShort(s) {
 	}
 	return shortest.length;
 }
+
+// Area of a Square
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+function squareArea(a){
+  let r= (a*2)/(Math.PI)
+  return Math.round(Math.pow(r,2)*100)/100
+}
