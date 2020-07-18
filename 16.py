2db869eef48ffebb5ae93e75cd5b33052aa265cb
Kaniah Dunn <kaniahdunn@gmail.com>
2020-07-18 09:28:16 -0400

    daily codewars

--- 16.py
+++ 16.py
@@ -23,3 +23,20 @@ def is_divisible(n,x,y):
         return True
     else:
         return False
+
+# Simple remove duplicatesI
+# n this Kata, you will remove the left-most duplicates from a list of integers and return the result.
+
+
+# # Remove the 3's at indices 0 and 3
+# # followed by removing a 4 at index 1
+# solve([3, 4, 4, 3, 6, 3]) # => [4, 6, 3]
+def solve(arr):
+    checker = []
+    reverse_checker = []
+    for i in reversed(arr):
+        if i not in checker:
+            checker.append(i)
+    for i in reversed(checker):
+        reverse_checker.append(i)
+    return reverse_checker
