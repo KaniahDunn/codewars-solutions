76aaa4ca059333265de672fffae15cfeb5f84111
Kaniah Dunn <kaniahdunn@gmail.com>
2020-07-22 22:12:54 -0400

    daily codewars

--- 16.py
+++ 16.py
@@ -107,3 +107,14 @@ def divisors(integer):
 # The middle character(s) of the word represented as a string.
 def get_middle(s):
     return s[(len(s)-1)//2:(len(s)+2)//2]
+
+# Is n divisible by x and y?
+# Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+# Examples:
+# 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+# 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+# 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+# 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+def is_divisible(n,x,y):
+    return n % x == 0 and n % y == 0
