4b88747a16d715700c1f3f752d2836f00bd9228a
Kaniah Dunn <kaniahdunn@gmail.com>
2020-07-29 18:47:18 -0400

    daily codewars

--- 17.py
+++ 17.py
@@ -114,3 +114,28 @@ def likes(names):
 # You should treat each array passed as a set of unique values for this kata.
 def powers(lst):
     return 2**len(lst)
+
+# reverseit
+# You have to create a function named reverseIt.
+
+# Write your function so that in the case a string or a number is passed in as the data , you will return the data in reverse order. If the data is any other type, return it as it is.
+
+# Examples of inputs and subsequent outputs:
+
+# "Hello" -> "olleH"
+
+# "314159" -> "951413"
+
+# [1,2,3] -> [1,2,3]
+def reverse_it(data):
+    if isinstance(data,bool):
+        return data
+    elif isinstance(data,int):
+        return int(str(data)[::-1])
+    elif isinstance(data,str):
+        return data[::-1]
+    elif isinstance(data,float):
+        return float(str(data)[::-1])
+
+    else:
+        return data
