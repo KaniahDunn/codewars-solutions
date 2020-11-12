4f127ddfa9b1c23709896e5a69f8885e0a3df140
Kaniah Dunn <kaniahdunn@gmail.com>
2020-11-12 15:15:15 -0500

    renaming files

---
+++ Javascript /a_bugs_trilogy_make_a_player.js
@@ -0,0 +1,28 @@
+// A (no more) bugs trilogy: Episode 3 - Make a Player
+
+// Pac succesfully passed the first & second test.
+// He just received the last message from the Academy,
+//
+// This is your last test, if you pass, your application will be accepted. You have 45 minutes.
+//
+// Good Luck
+//
+// Pac has to create a Football Player object with the following attributes :
+//
+// name      -> (string)
+// position  -> (string)
+// age       -> (int)
+// dribbling -> (int)
+// pass      -> (int)
+// shoot     -> (int)
+
+function Player(name, position, age, dribbling, pass, shoot) {
+    // Have fun!
+    this.name = name
+    this.position = position
+    this.age = age
+    this.dribbling = dribbling
+    this.pass = pass
+    this.shoot = shoot
+  }
+
---
+++ Javascript /opposite_number.js
@@ -0,0 +1,10 @@
+//Opposite Number
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+// 1: -1
+// 14: -14
+// -34: 34
+var opposite = number => -(number)
+
