9ef98a549476adbab419301c8b9d872ebe958b2b
Kaniah Dunn <kaniahdunn@gmail.com>
2020-11-12 14:38:00 -0500

    renaming files

--- Javascript /nba_full_48_min_average.js
+++ Javascript /nba_full_48_min_average.js
@@ -1,10 +1,3 @@
-//Solution 1
-//Order of Operations
-
-function orderOperations () {
-  return (2 + 2) * ((2 + 2) * 2)
-}
-
 //Solution 2
 //NBA Full 48 Min Average
 
---
+++ Javascript /reversed_strings.js
@@ -0,0 +1,7 @@
+//Reversed Strings
+//Complete the solution so that it reverses the string value passed into it.
+
+
+function solution(str) {
+  return str.split("").reverse().join('');
+}
