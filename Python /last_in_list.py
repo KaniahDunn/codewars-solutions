8437e4719196e803df7b44979a6d4b2ca9ef31c6
Kaniah Dunn <kaniahdunn@gmail.com>
2021-01-04 20:32:44 -0500

    solution

---
+++ Python /last_in_list.py
@@ -0,0 +1,9 @@
+# Last in list
+#  99 Problems, #1: last in list
+# Write a function last that accepts a list and returns the last element in the list.
+# If the list is empty:
+# In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+# In languages that do not have an empty option, just return None
+def last(lst):
+    if lst == []:return None
+    else:return lst[-1]gi
