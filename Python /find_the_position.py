abe5ab844f277283caf3ed4abf653909178225c7
Kaniah Dunn <kaniahdunn@gmail.com>
2021-01-07 22:27:59 -0500

    codewars solution

---
+++ Python /find_the_position.py
@@ -0,0 +1,11 @@
+"""
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+This kata is meant for beginners. Rank and upvote to bring it out of beta
+"""
+def position(alphabet):
+    return f"Position of alphabet: {ord(alphabet) - 96}"
